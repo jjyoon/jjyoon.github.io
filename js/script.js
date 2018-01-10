197ab5ffd8d935d88ece8d227d39a6e507fedcd7
Jimmy Yoon <jim.yoon93@gmail.com>
2018-01-10 11:53:54 -0600

    Update pinterest movie 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.c-nav__menu-button')
 const whiteMenuButton = document.querySelector('.c-nav__menu-button--white-bg')
 const menu = document.querySelector('.c-nav__menu')
+const video = document.querySelector('video')
 
 toggleOpen = () => {
     menu.classList.toggle('is-hidden');
@@ -10,4 +11,10 @@ if (menuButton) {
     menuButton.addEventListener("click", toggleOpen);
 } else {
     whiteMenuButton.addEventListener("click", toggleOpen);
+}
+
+if (video) {
+    video.addEventListener('click', function () {
+        video.play();
+    }, false);
 }
